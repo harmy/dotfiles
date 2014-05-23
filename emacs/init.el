e5883c3160bd74052f125fdfc20d101f21a67532
harmy <hack4cn@gmail.com>
2014-05-23 22:51:29 +0800

    replace elpy with jedi

--- emacs/init.el
+++ emacs/init.el
@@ -156,6 +156,11 @@
 (setq js-indent-level 2)
 
 
+;; Jedi
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:setup-keys t)
+(setq jedi:complete-on-dot t)
+
 ;; ==================================================
 ;;              CUSTOM FUNCTIONS
 ;; ==================================================
